ba5c7a067b20152289926bfc38780be55630b0b1
Aaron <aaroncallaghan40@gmail.com>
2020-07-30 17:34:00 +0100

    Don't have sim/validate modes working yet

--- min-script.py
+++ min-script.py
@@ -2,49 +2,12 @@ import sys, subprocess, os, shutil
 import multiprocessing as mp
 from pathlib import Path
 
-# ----------------Terminal calls for each Bigrapher argument----------------#
 
-# Set to True if tmin only enabled
-tmin_mode = False
-
-
-def start(input_dir, output_dir, mode):
-
-    # Assume 5 arguments passed only when running tmin on 1 bigrapher mode
-    # if args_len == 5:
-    #   if sys.argv[4] == 'tmin':
-
-    # Checks which bigrapher mode to run
-    if args_len == 4:
-
-        # Checks for and calls relevant Bigrapher arguments
-        if (mode == 'full'):
-
-            full(input_dir, output_dir)
-        elif (mode == 'sim'):
-
-            args_list = sim('afl-cmin', input_dir, output_dir)
-        elif (mode == 'validate'):
-
-            args_list = validate('afl-cmin', input_dir, output_dir)
-        else:
-
-            print('invalid bigrapher flag please select on of the following: sim, full, val followed by input and output directory')
-
-
-
-
-# Returns template for computation minimisation
-def full(input, output):
-    # if(tmin_mode == False):
-
-    #     subprocess.run(['afl-cmin', '-i', input, '-o', output, 'bigrapher', 'full', '-p', '/dev/null', '@@'], check=True)
-
-
-    ### Splits up files into smaller directories for tmin-script
-
-    # Creates an list of all files in output_directory
-    corpus_files = os.listdir(output)
+# ----------------Splits large corpus into smaller directories for tmin-script----------------#
+def split_corpus(corpus_dir):
+    ### Splits up files into smaller directories for tmin-script ###
+    # Creates an list of all files in the corpus directory
+    corpus_files = os.listdir(corpus_dir)
     # Stores number of threads to be made (will use all cores on CPU)
     threads = mp.cpu_count()
 
@@ -63,56 +26,103 @@ def full(input, output):
     for i in range(0, threads):
 
         # Creates new directory
-        new_dir = os.path.join(output, 'full' + str(i))
+        new_dir = os.path.join(corpus_dir, 'full' + str(i))
         os.mkdir(new_dir)
+        # Adds new directory to directory list
         dir_list.append(new_dir)
 
-        # Moves fraction(no_of_seeds) of files into new directory
+        # Moves fraction of files (no_of_seeds or number of remaining files) into new directory
         for i in range(0, min(no_of_seeds, len(corpus_files))):
-            old_dir = os.path.join(output, corpus_files.pop())
+
+            old_dir = os.path.join(corpus_dir, corpus_files.pop())
             shutil.move(old_dir, new_dir)
+    
+    return dir_list
 
+
+# ----------------Bigrapher arguments for each mode----------------#
+# Returns template for computation minimisation
+def full(input_dir, output_dir, target):
+
+    # Minimised corpus size to minimum number of relevant files
+    subprocess.run(['afl-cmin', '-i', input_dir, '-o', output_dir, target, 'full', '-p', '/dev/null', '@@'], check=True)
+
+    # Splits up main corpus dirtectory into smaller directories for tmin-script and stores list of new directories
+    dir_list = split_corpus(output_dir)
+
+    # Opens new terminal window to run tmin script
+    # Note: new terminals opened as tmin runs 1 process per terminal and will use 1 core per process 
+    # (this is the best way to do asynchronus tmin operations)
     for d in dir_list:
-
-
-        subprocess.call(['gnome-terminal', '-e', 'python3 /home/r0qu3/tmin-script.py ' + d  + ' full'])
-
-    # running_procs = [subprocess.Popen(['gnome-terminal -e bash python3 /home/r0qu3/tmin-script.py ' + str(d)], shell=True) for d in dir_list]
-
-    # while running_procs:
-    #     for proc in running_procs:
-
-    #         returncode = proc.poll()
-    #         if returncode is not None:
-    #             running_procs.remove(proc)
-    #             print("Process " + str(proc.pid) + " is finished!")
-    #             break
+        # Calls tmin script with relevant bigrapher mode
+        subprocess.call(['gnome-terminal', '-e', 'python3 /home/r0qu3/tmin-script.py ' + output_dir + ' ' + d  + ' ' + target + ' full'])
 
 
 # Returns template for simulation minimisation
-# def siminput, output):
+def sim(input_dir, output_dir, target):
 
-# return [mode, '-i', input, '-o', output, 'bigrapher', 'sim', '-T', '500', '-s', '-t', '/dev/null', '-f', 'svg', '@@']
+    # Minimised corpus size to minimum number of relevant files
+    subprocess.run(['afl-cmin', '-i', input_dir, '-o', output_dir, target, 'sim', '-T', '500', '-s', '-t', '/dev/null', '-f', 'svg', '@@'], check=True)
+
+    # Splits up main corpus dirtectory into smaller directories for tmin-script and stores list of new directories
+    dir_list = split_corpus(output_dir)
+
+    # Opens new terminal window to run tmin script
+    # Note: new terminals opened as tmin runs 1 process per terminal and will use 1 core per process 
+    # (this is the best way to do asynchronus tmin operations)
+    for d in dir_list:
+        # Calls tmin script with relevant bigrapher mode
+        subprocess.call(['gnome-terminal', '-e', 'python3 /home/r0qu3/tmin-script.py ' + output_dir + ' ' + d  + ' sim'])
+    
 
 
 # Returns template for validation minimisation
-# def validate(input, output):
+def validate(input_dir, output_dir, target):
 
-# return [mode, '-i', input, '-o', output, 'bigrapher', 'validate', '-d', '/dev/null', '-f', 'svg', '@@']
+    # Minimised corpus size to minimum number of relevant files
+    subprocess.run(['afl-cmin', '-i', input_dir, '-o', output_dir, target, 'validate', '-d', '/dev/null', '-f', 'svg', '@@'], check=True)
+
+    # Splits up main corpus dirtectory into smaller directories for tmin-script and stores list of new directories
+    dir_list = split_corpus(output_dir)
+
+    # Opens new terminal window to run tmin script
+    # Note: new terminals opened as tmin runs 1 process per terminal and will use 1 core per process 
+    # (this is the best way to do asynchronus tmin operations)
+    for d in dir_list:
+        # Calls tmin script with relevant bigrapher mode
+        subprocess.call(['gnome-terminal', '-e', 'python3 /home/r0qu3/tmin-script.py ' + output_dir + ' ' + d  + ' validate'])
+
+
 
 # ----------------Argument Parsing----------------#
-
 # Note: terminal takes script call as argv[0]: "python3 min-script.py input output [bigrapher-mode] [tmin-option]"
 args_len = len(sys.argv)
 
-if (args_len != 4):
+# If not enough information passed to program, warn user
+if (args_len < 4):
 
-    print("Invalid input. Script needs an input and output directory.")
+    print("Invalid input. Script needs an input directory, an output directory and the executables location.")
 else:
 
     # Input and output directories for pruning
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
-    # Sets bigrapher mode
-    mode = sys.argv[3]
-    start(input_dir, output_dir, mode)
+    # Stores the location of instrumented .exe
+    target = sys.argv[3]
+    # 5th argument always bigrapher mode
+    if args_len == 5:
+        # Sets bigrapher mode
+        mode = sys.argv[4]
+        # Checks for and calls relevant Bigrapher arguments
+        if (mode == 'full'):
+
+            full(input_dir, output_dir, target)
+        elif (mode == 'sim'):
+
+            sim(input_dir, output_dir, target)
+        elif (mode == 'validate'):
+
+            validate(input_dir, output_dir, target)
+        else:
+
+            print('Invalid bigrapher flag please select on of the following: sim, full, val followed by input and output directory')
--- tmin-script.py
+++ tmin-script.py
@@ -1,10 +1,32 @@
-import sys, subprocess, os
+from subprocess import run
+from shutil import move
+from sys import argv
+from os import listdir, scandir, path, rmdir
 
+
+#---------------Handles moving files back to parent directory---------------#
+# Note: afl expects 1 corpus directory of seed files, this function returns all minimised files to main corpus directory
+def copy_back(corpus_directory, parent_directory):
+     # Stores seed files as list instead of iterable as allows for easier path creation
+    seed_files = listdir(corpus_directory)
+
+    # Loops through corpus directory and copies 
+    for seed in seed_files:
+        # Creates path of file to be moved
+        old_dir = path.join(corpus_directory, seed)
+        # Moves file to main corpus directory
+        move(old_dir, parent_directory)
+
+
+    rmdir(corpus_directory)
 #---------------Sets up tmin call for each mode---------------#
-def full(seed_files, total_files):   
+def full(corpus_directory, parent_directory, total_files):  
+
+    # Stores an iterator of DirEntry objects
+    seed_files = scandir(corpus_directory)
     # Initialises counter
     i = 0     
-            
+       
     # Iterates through all files in directory and reduces them to the smallest number of useful bits
     for seed in seed_files:
         # Increments counter
@@ -13,67 +35,85 @@ def full(seed_files, total_files):
         print('minimising ' + str(i) + '/' + str(total_files) + '\n')
 
         # Calls afl-tmin
-        subprocess.run(['afl-tmin', '-i', seed, '-o', seed, 'bigrapher', 'full', '-p', '/dev/null', '@@'])
+        run(['afl-tmin', '-i', seed, '-o', seed, target, 'full', '-p', '/dev/null', '@@'])
             
-    print('Tmin process completed! Have a nice day!')
+    copy_back(corpus_directory, parent_directory)
+
+    print('Tmin process completed! Have a nice day! :)')
 
 
 
 
-def sim(seed_files, total_files): 
-    # Initialises counter
-    i = 0        
-         
-    # Iterates through all files in directory and reduces them to the smallest number of useful bits
-    for seed in seed_files:
-        # Increments counter
-        i += 1
-        # Prints test number current-test/total-tests
-        print('minimising ' + str(i) + '/' + str(total_files) + '\n')
-        
-    print('Tmin process completed! Have a nice day!')
+def sim(corpus_directory, parent_directory, total_files): 
 
-
-
-
-def validate(seed_files, total_files):    
+    # Stores an iterator of DirEntry objects
+    seed_files = scandir(corpus_directory)
     # Initialises counter
     i = 0     
-            
+       
     # Iterates through all files in directory and reduces them to the smallest number of useful bits
     for seed in seed_files:
         # Increments counter
         i += 1
         # Prints test number current-test/total-tests
         print('minimising ' + str(i) + '/' + str(total_files) + '\n')
-        
-    print('Tmin process completed! Have a nice day!')
+
+        # Calls afl-tmin
+        run(['afl-tmin', '-i', seed, '-o', seed, target, 'sim', '-T', '500', '-s', '-t', '/dev/null', '-f', 'svg', '@@'])
+            
+    copy_back(corpus_directory, parent_directory)
+
+    print('Tmin process completed! Have a nice day! :)')
+
+
+def validate(corpus_directory, parent_directory, total_files):  
+ 
+    # Stores an iterator of DirEntrseed_filesy objects
+    seed_files = scandir(corpus_directory)
+    # Initialises counter
+    i = 0     
+       
+    # Iterates through all files in directory and reduces them to the smallest number of useful bits
+    for seed in seed_files:
+        # Increments counter
+        i += 1
+        # Prints test number current-test/total-tests
+        print('minimising ' + str(i) + '/' + str(total_files) + '\n')
+
+        # Calls afl-tmin
+        run(['afl-tmin', '-i', seed, '-o', seed, target, 'validate', '-d', '/dev/null', '-f', 'svg', '@@'])
+            
+    copy_back(corpus_directory, parent_directory)
+
+    print('Tmin process completed! Have a nice day! :)')
 
 
 
 
 #---------------Parsing input---------------#
+# Stores the location of the parent directory to copy minimised files to
+parent_directory = argv[1]
 # Stores the location of the corpus_directory
-corpus_directory = sys.argv[1]
+corpus_directory = argv[2]
+# Stores location of target .exe
+target = argv[3]
 # Stores the bigrapher mode to be tested
-mode = sys.argv[2]
-
-# Stores an iterator of DirEntry objects
-seed_files = os.scandir(corpus_directory)
+mode = argv[4]
+ 
 # Stores the number of seed files
-total_files = len(os.listdir(corpus_directory))
+total_files = len(listdir(corpus_directory))
 
 
 #---------------Calling Correct Mode---------------#
 if mode == 'full':
 
-    full(seed_files, total_files)
+    full(corpus_directory, parent_directory, total_files)
 elif mode == 'sim':
 
-    sim(seed_files, total_files)
+    sim(corpus_directory, parent_directory, total_files)
 elif mode == 'validate':
 
-    validate(seed_files, total_files)
+    validate(corpus_directory, parent_directory, total_files)
 else:
 
     print('Invalid Bigrapher mode please select full, sim or validate.')
